b6fe05e7fd35387e0dcebef60c1381ef1ec291f3
Subhash Mangipudi <sb.mangipudi@outlook.com>
2024-12-30 18:13:22 +0530

    Fixing the tests

--- Cashflows/income-expense-scheduler/tests/test_main.py
+++ Cashflows/income-expense-scheduler/tests/test_main.py
@@ -7,7 +7,7 @@ import os
 # Add the src directory to the PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from src.main import main
+from main import main
 
 class TestMain(unittest.TestCase):
     @patch('builtins.open', new_callable=mock_open, read_data='{"initial_portfolio_value": 100000, "end_analysis_year": 2050, "retirement_year": 2030, "pre_retirement_tax": 0.25, "post_retirement_tax": 0.15, "equity_allocation": 0.6, "fixed_income_allocation": 0.4, "lifestyle_expenses": 30000, "data": [{"years": "2023-2025", "savings": 20000, "withdrawals": 10000, "post_tax": true}]}')
@@ -16,7 +16,7 @@ class TestMain(unittest.TestCase):
     def test_main(self, mock_json_load, mock_open):
         mock_json_load.side_effect = [
             json.loads('{"initial_portfolio_value": 100000, "end_analysis_year": 2050, "retirement_year": 2030, "pre_retirement_tax": 0.25, "post_retirement_tax": 0.15, "equity_allocation": 0.6, "fixed_income_allocation": 0.4, "lifestyle_expenses": 30000, "data": [{"years": "2023-2025", "savings": 20000, "withdrawals": 10000, "post_tax": true}]}'),
-            json.loads('[{"scenario_id": 1, "years": [{"year": 2023, "equity_return": 0.1, "fixed_income_return": 0.05}, {"year": 2024, "equity_return": 0.1, "fixed_income_return": 0.05}, {"year": 2025, "equity_return": 0.1, "fixed_income_return": 0.05}]}]')
+            json.loads('[{"scenario_id": 1, "years": [{"year": 2024, "equity_return": 0.1, "fixed_income_return": 0.05}, {"year": 2025, "equity_return": 0.1, "fixed_income_return": 0.05}, {"year": 2026, "equity_return": 0.1, "fixed_income_return": 0.05}]}]')
         ]
         with patch('builtins.print') as mock_print:
             main('input.json', 'scenarios.json')
